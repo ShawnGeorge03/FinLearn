d77657603d4e3ed6ceb5b4e5ebc3fb21e4a28ff0
ShawnGeorge03 <32755895+ShawnGeorge03@users.noreply.github.com>
2023-07-29 15:36:05 -0400

    feat: ✨ test pushing new changes

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -4,5 +4,5 @@ import styles from '@/styles/pages/Home.module.scss';
 
 export default function Home() {
   const { title } = styles;
-  return <h1 className={title}>Hello</h1>;
+  return <h1 className={title}>Hello World. This is the Latest Change</h1>;
 }
